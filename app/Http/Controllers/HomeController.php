45b975e8f03477871b163a93465ef59cbcf5b0cd
persontechnology <persontechnologys@gmail.com>
2022-01-12 11:39:29 -0500

    edit home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,36 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-          
-        // enviar por mensaje
-      try {
-        $basic  = new \Vonage\Client\Credentials\Basic("4c46a3a0", "cLk1nYZNbiKCHhJr");
-        $client = new \Vonage\Client($basic);
-        $response = $client->sms()->send(
-            new \Vonage\SMS\Message\SMS("593998808775", 'CACTU', 'A text message sent using the Nexmo SMS API')
-        );
-        
-        $message = $response->current();
-        
-        if ($message->getStatus() == 0) {
-            echo "The message was sent successfully\n";
-        } else {
-            echo "The message failed with status: " . $message->getStatus() . "\n";
-        }
-
-        $message = $response->current();
-
-        if ($message->getStatus() == 0) {
-            return "The message was sent successfully\n";
-        } else {
-            return "The message failed with status: " . $message->getStatus() . "\n";
-        }
-
-      } catch (\Throwable $th) {
-        return $th->getMessage();
-      }
-
-
-        //return view('home');
+        return view('home');
     }
 }
